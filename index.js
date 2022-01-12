b975b8c1ab1ce4a1d7cfc1692f1d3c22c989fd8f
Kristen <kriscadacio@gmail.com>
2022-01-12 13:35:27 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Kristen Cadacio", 
+    streetAddress1: "28-05 37th Street New York, New York"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    var newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    var newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
